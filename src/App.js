0504271a1d10f50c3ee377aeda91bbf8bb7b465f
Tariqali <Tariqali99.ta@gmail.com>
2019-08-15 16:26:58 +0500

    firstcomit

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
+        <Router basename="/">
           <Switch>
             <Route path='/' exact component={Home}></Route>
             {/* <Route path='/Home' exact component={Home}></Route> */}
